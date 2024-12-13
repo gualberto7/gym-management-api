eb42788e9094de57d7649a95a770f38e40c0558c
gcuiza <gualbertocuizav@gmail.com>
2024-12-13 14:36:22 -0400

    refactored to use macros

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -10,23 +10,7 @@ class MemberController extends Controller
 {
     public function index(Request $request)
     {
-        $members = Member::query();
-        $sortField = $request->input('sort');
-        $sortDirection = 'asc';
-        $allowedSorts = ['name'];
-        if ($request->filled('sort')) {
-            if (Str::of($sortField)->startsWith('-')) {
-                $sortDirection = 'desc';
-                $sortField = Str::of($sortField)->substr(1);
-            }
-            if (!in_array($sortField, $allowedSorts)) {
-                abort(400, 'Invalid sort field');
-            }
-
-            $members->orderBy($sortField, $sortDirection);
-        }
-
-        $members = $members->get();
+        $members = Member::allowedSorts(['name'])->get();
         return response()->json($members, 200);
     }
 
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,7 +23,25 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
-            return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+            return config('app.frontend_url') . "/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+        });
+
+        Builder::macro('allowedSorts', function ($allowedSorts) {
+            return $this->when(request()->filled('sort'), function ($query) use ($allowedSorts) {
+                $sortField = request()->input('sort');
+                $sortDirection = 'asc';
+
+                if (Str::of($sortField)->startsWith('-')) {
+                    $sortDirection = 'desc';
+                    $sortField = Str::of($sortField)->substr(1);
+                }
+
+                if (!in_array($sortField, $allowedSorts)) {
+                    abort(400, 'Invalid sort field');
+                }
+
+                return $query->orderBy($sortField, $sortDirection);
+            });
         });
     }
 }
