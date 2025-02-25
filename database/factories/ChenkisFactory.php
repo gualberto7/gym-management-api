b2c50d64808f33df5028ba1b003c2cd88d17bbd5
gcuiza <gualbertocuizav@gmail.com>
2025-02-25 11:30:36 -0400

    Add chenkis model, migration and factory

---
+++ database/factories/ChenkisFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Chenkis>
+ */
+class ChenkisFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'member_id' => App\Models\Member::factory(),
+            'gym_id' => App\Models\Gym::factory(),
+            'registred_by' => $this->faker->name(),
+        ];
+    }
+}
