7d1c4d5c0a0f7c41f0b2d22ed7ff0ab86da584fd
gcuiza <gualbertocuizav@gmail.com>
2025-02-25 19:25:31 -0400

    Add chenkis resource

---
+++ app/Http/Resources/ChenkisResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ChenkisResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'member_name' => $this->member->name,
+            'member_phone' => $this->member->phone,
+            'created_at' => $this->created_at->toDateTimeString(),
+            'created_by' => $this->created_by,
+        ];
+    }
+}
