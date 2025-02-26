b2a464f62ffdd4eec046a51b8f02ad9371abd01e
gcuiza <gualbertocuizav@gmail.com>
2025-02-26 12:04:27 -0400

    can find subscription based of member's ci

---
+++ tests/Feature/Subscription/FindSubscriptionTest.php
@@ -0,0 +1,67 @@
+<?php
+
+use App\Models\Member;
+
+test('guests users cannot find subscriptions', function () {
+    $response = $this->getJson(route('api.subscribed-members.show', 1));
+
+    $response->assertStatus(401);
+});
+
+test('admins can find a subscription', function () {
+    $data = $this->createUserGymMembershipAndSubscription('admin');
+
+    $response = $this->actingAs($data['user'])
+        ->getJson(route('api.subscribed-members.show', $data['member']->ci));
+
+    $response->assertStatus(200);
+});
+
+test('owners can find a subscription', function () {
+    $data = $this->createUserGymMembershipAndSubscription('admin');
+
+    $response = $this->actingAs($data['user'])
+        ->getJson(route('api.subscribed-members.show', $data['member']->ci));
+
+    $response->assertStatus(200);
+});
+
+test('verify the response structure when finding a subscription', function () {
+    $data = $this->createUserGymMembershipAndSubscription('admin');
+
+    $response = $this->actingAs($data['user'])
+        ->getJson(route('api.subscribed-members.show', $data['member']->ci));
+
+    $response->assertJsonStructure([
+        'data' => [
+            'id',
+            'member',
+            'email',
+            'phone',
+            'membership',
+            'start_date',
+            'end_date',
+        ],
+    ]);
+});
+
+test('it fails if the member does not exist', function () {
+    $data = $this->createUserGymMembershipAndSubscription('admin');
+
+    $response = $this->actingAs($data['user'])
+        ->getJson(route('api.subscribed-members.show', '123456'));
+
+    $response->assertStatus(404);
+    $response->assertJsonPath('message', 'No hay registros para CI: 123456');
+});
+
+test('it fails if the member does not have a subscrition', function () {
+    $data = $this->createUserGymMembership('admin');
+    $member = Member::factory()->create();
+
+    $response = $this->actingAs($data['user'])
+        ->getJson(route('api.subscribed-members.show', $member->ci));
+
+    $response->assertStatus(404);
+    $response->assertJsonPath('message', 'Este usuario no tiene una subscripción');
+});
