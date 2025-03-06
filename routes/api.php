a11152395423b7b66470ae27d9ea21851a59cd38
gcuiza <gualbertocuizav@gmail.com>
2025-03-05 21:29:26 -0400

    refactor imports

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,5 @@
 <?php
 
-use App\Http\Controllers\EntryController;
-use App\Http\Controllers\GymController;
-use App\Http\Controllers\MemberController;
-use App\Http\Controllers\MembershipController;
-use App\Http\Controllers\SubscriptionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,7 +7,7 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 })->name(('api.user.find'));
 
-Route::controller(SubscriptionController::class)
+Route::controller(App\Http\Controllers\SubscriptionController::class)
     ->middleware('auth:sanctum')
     ->group(function () {
         Route::get('/subscriptions/{gymId}', 'index')->name('api.subscriptions.index')->middleware('role:admin|owner');
@@ -20,7 +15,7 @@ Route::controller(SubscriptionController::class)
         Route::post('/subscribed-members', 'store')->name('api.subscribed-members.store');
     });
 
-Route::controller(MemberController::class)
+Route::controller(App\Http\Controllers\MemberController::class)
     ->middleware('auth:sanctum')
     ->group(function () {
         Route::get('/members', 'index')->name('api.members.index');
@@ -28,21 +23,21 @@ Route::controller(MemberController::class)
         Route::post('/members', 'store')->name('api.members.store');
     });
 
-Route::controller(EntryController::class)
+Route::controller(App\Http\Controllers\EntryController::class)
     ->middleware('auth:sanctum')
     ->group(function () {
         Route::get('/entries', 'index')->name('api.entries.index');
         Route::post('/entries', 'store')->name('api.entries.store');
     });
 
-Route::controller(MembershipController::class)
+Route::controller(App\Http\Controllers\MembershipController::class)
     ->middleware('auth:sanctum')
     ->group(function () {
         Route::get('/memberships', 'index')->name('api.memberships.index');
         Route::post('/memberships', 'store')->name('api.memberships.store');
     });
 
-Route::controller(GymController::class)
+Route::controller(App\Http\Controllers\GymController::class)
     ->middleware('auth:sanctum')
     ->group(function () {
         Route::get('/gyms', 'index')->name('api.gyms.index')->middleware('role:admin|owner');
