7305c0c0beb1207ad32726d6d766889f128aa883
gcuiza <gualbertocuizav@gmail.com>
2025-03-17 09:28:11 -0400

    Fix create membership method

--- app/Http/Controllers/MembershipController.php
+++ app/Http/Controllers/MembershipController.php
@@ -20,11 +20,25 @@ class MembershipController extends Controller
 
     public function store($gymId, Request $request)
     {
-        if(!auth()->user()->allowedGym($gymId)) {
+        $user = $request->user();
+
+        if(!$user->allowedGym($gymId)) {
             abort(403);
         }
 
-        $membership = Membership::create($request->all());
+        $membership = Membership::create([
+            'name' => $request->name,
+            'price' => $request->price,
+            'duration' => $request->duration,
+            'duration_unit' => $request->duration_unit,
+            'description' => $request->description,
+            'max_checkins' => $request->max_checkins,
+            'active' => $request->active,
+            'gym_id' => $gymId,
+            'created_by' => $user->name,
+            'updated_by' => $user->name,
+        ]);
+
         return response()->json($membership, 201);
     }
 
